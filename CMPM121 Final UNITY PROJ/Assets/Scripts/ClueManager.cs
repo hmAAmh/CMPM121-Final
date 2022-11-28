51a2febc685e24e0c0f6ef8a639d479447997053
Jarrett-Mao <74739111+Jarrett-Mao@users.noreply.github.com>
2022-11-27 16:09:17 -0800

    Glowing clue cube implemented

---
+++ CMPM121 Final UNITY PROJ/Assets/Scripts/ClueManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClueManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject clueOne;
+    public bool clueOneOn = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
