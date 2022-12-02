aa479742e7ff1929a7e05aba658c3d93264bfc2e
Jarrett-Mao <74739111+Jarrett-Mao@users.noreply.github.com>
2022-12-01 21:01:36 -0800

    clue-player proximity detection works

--- CMPM121 Final UNITY PROJ/Assets/Scripts/ClueInteract.cs
+++ CMPM121 Final UNITY PROJ/Assets/Scripts/ClueInteract.cs
@@ -12,12 +12,19 @@ public class ClueInteract : MonoBehaviour
 
     private ClueManager clueManager;
 
+    //variables for glow effect
     private PostProcessVolume ppVolume;
     private Bloom bloom;
+
+    //proximity detection variables
+    public float detectionRange;
+    public bool closeEnough = false;
+    public Transform playerLocation;
 
     // Start is called before the first frame update
     void Start()
     {
+        //assign glow effect variables
         ppVolume = GetComponent<PostProcessVolume>();
         ppVolume.profile.TryGetSettings(out bloom);
         clueManager = clueManagerGameObject.GetComponent<ClueManager>();
@@ -28,17 +35,37 @@ public class ClueInteract : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(playerLocation.position, transform.position) <= detectionRange){
+            closeEnough = true;
+        }
+        else {
+            closeEnough = false;
+        }
+
+        // Debug.Log("Player: " + playerLocation.position);
+        // Debug.Log("Clue: " + transform.position);
+        // if (closeEnough == true){
+        //     Debug.Log("In range!");
+        // }
+        // else {
+        //     Debug.Log("Not in range!");
+        // }
     }
 
-    void OnMouseDown(){
-        gameObject.GetComponent<MeshRenderer>().material = mat1;
-        StartCoroutine(lightUp());
-        clueManager.clueOneOn = true;
-        Debug.Log("Clue One Status: " + clueManager.clueOneOn);
+    void OnMouseDown(){ //swap materials and turn on lightup 
+        if (closeEnough == false){
+            return;
+        }
+        else {
+            gameObject.GetComponent<MeshRenderer>().material = mat1;
+            StartCoroutine(lightUp());
+            clueManager.clueOneOn = true;
+            Debug.Log("Clue One Status: " + clueManager.clueOneOn);
+        }
+  
     }
 
-    IEnumerator lightUp(){ //changes intensity over time
+    IEnumerator lightUp(){ //changes intensity of glow effect over time
         float bloomVal = bloom.intensity.value;
         for(float intensity = bloomVal; intensity < 20; intensity++){
             bloom.intensity.value = intensity;
